2ff24e1c1a23e6f4b56f68bb29810389f5a2dfb5
Rami Reshad <152604548+Ramram213@users.noreply.github.com>
2025-07-16 15:16:08 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBAc69ln_oIq2qGLQxGxk4UUuW2y_mcU9M",
-  authDomain: "inventory-management-61c59.firebaseapp.com",
-  projectId: "inventory-management-61c59",
-  storageBucket: "inventory-management-61c59.appspot.com",
-  messagingSenderId: "141286258082",
-  appId: "1:141286258082:web:43fa68b695d842b168aefb",
-  measurementId: "G-3G5KXHCX6J"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
